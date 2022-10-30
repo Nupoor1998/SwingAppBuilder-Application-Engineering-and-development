c80008c28e0f55733df865566b6f176ddcc18caf
nupoorkorde <nupoorkorde@10.110.176.189>
2022-10-30 12:41:40 -0400

    first

---
+++ AED_ASS2/src/aed_ass2/City.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aed_ass2;
+
+/**
+ *
+ * @author anupamaditya
+ */
+public class City {
+    
+    private String City;
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+    
+    
+}
